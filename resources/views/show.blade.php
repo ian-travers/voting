54e6ae8e6def6ec322a05584bd256aee9ce9c4f2
Ian Travers <mr.itpe@gmail.com>
2021-04-07 10:31:22 +0300

    Idea page #3: add three comments (one of them  is an admin comment)

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -94,4 +94,154 @@
             </button>
         </div>
     </div><!-- End buttons container -->
+
+    <div class="comments-container ml-22 space-y-6 my-6">
+
+        <div class="comment-container bg-white rounded-lg flex mt-4">
+            <div class="flex flex-1 px-4 py-6">
+                <div class="flex-none">
+                    <a href="###">
+                        <img src="https://source.unsplash.com/200x200/?face&crop=face&v=2" alt="avatar"
+                             class="w-14 h-14 rounded-lg">
+                    </a>
+                </div>
+
+                <div class="mx-4 w-full">
+                    <div class="text-gray-600">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem error iure magnam magni obcaecati quisquam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum laborum rem repudiandae. Accusamus amet atque est harum, iste odio tempora? Eveniet, voluptatibus.
+                    </div>
+
+                    <div class="flex items-center justify-between mt-6">
+                        <div class="flex items-center text-xs font-semibold space-x-2 text-gray-400">
+                            <div class="font-bold text-gray-900">John Doe</div>
+                            <div>&bull;</div>
+                            <div>10 hours ago</div>
+                        </div>
+
+                        <div class="relative">
+                            <button
+                                class="bg-gray-100 hover:bg-gray-200 rounded-full border h-7 px-4 py-2 transition duration-150 ease-in">
+                                <svg class="text-gray-400 hover:text-gray-500" fill="currentColor" width="24"
+                                     height="6">
+                                    <path
+                                        d="M2.97.061A2.969 2.969 0 000 3.031 2.968 2.968 0 002.97 6a2.97 2.97 0 100-5.94zm9.184 0a2.97 2.97 0 100 5.939 2.97 2.97 0 100-5.939zm8.877 0a2.97 2.97 0 10-.003 5.94A2.97 2.97 0 0021.03.06z"></path>
+                                </svg>
+                            </button>
+                            <ul class="hidden absolute w-40 bg-white font-semibold shadow-dialog rounded-lg py-2 ml-12">
+                                <li>
+                                    <a href="#"
+                                       class="block hover:bg-gray-100 transition duration-150 ease-in px-5 py-2">Mark as
+                                        Spam</a>
+                                </li>
+                                <li>
+                                    <a href="#"
+                                       class="block hover:bg-gray-100 transition duration-150 ease-in px-5 py-2">Delete
+                                        Post</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div><!-- End comment container -->
+        <div class="comment-container bg-white rounded-lg flex mt-4">
+            <div class="flex flex-1 px-4 py-6">
+                <div class="flex-none">
+                    <a href="###">
+                        <img src="https://source.unsplash.com/200x200/?face&crop=face&v=3" alt="avatar"
+                             class="w-14 h-14 rounded-lg">
+                    </a>
+                    <div class="text-center text-blue text-xxs font-bold uppercase mt-1">Admin</div>
+                </div>
+
+                <div class="mx-4 w-full">
+                    <h4 class="text-xl font-semibold">
+                        <a href="#" class="hover:underline">Status changed to "Under consideration"</a>
+                    </h4>
+                    <div class="text-gray-600 mt-4">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum laborum rem repudiandae. Accusamus amet atque est harum, iste odio tempora? Eveniet, voluptatibus.
+                    </div>
+
+                    <div class="flex items-center justify-between mt-6">
+                        <div class="flex items-center text-xs font-semibold space-x-2 text-gray-400">
+                            <div class="font-bold text-blue">Adrean</div>
+                            <div>&bull;</div>
+                            <div>10 hours ago</div>
+                        </div>
+
+                        <div class="relative">
+                            <button
+                                class="bg-gray-100 hover:bg-gray-200 rounded-full border h-7 px-4 py-2 transition duration-150 ease-in">
+                                <svg class="text-gray-400 hover:text-gray-500" fill="currentColor" width="24"
+                                     height="6">
+                                    <path
+                                        d="M2.97.061A2.969 2.969 0 000 3.031 2.968 2.968 0 002.97 6a2.97 2.97 0 100-5.94zm9.184 0a2.97 2.97 0 100 5.939 2.97 2.97 0 100-5.939zm8.877 0a2.97 2.97 0 10-.003 5.94A2.97 2.97 0 0021.03.06z"></path>
+                                </svg>
+                            </button>
+                            <ul class="hidden absolute w-40 bg-white font-semibold shadow-dialog rounded-lg py-2 ml-12">
+                                <li>
+                                    <a href="#"
+                                       class="block hover:bg-gray-100 transition duration-150 ease-in px-5 py-2">Mark as
+                                        Spam</a>
+                                </li>
+                                <li>
+                                    <a href="#"
+                                       class="block hover:bg-gray-100 transition duration-150 ease-in px-5 py-2">Delete
+                                        Post</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div><!-- End comment container -->
+        <div class="comment-container bg-white rounded-lg flex mt-4">
+            <div class="flex flex-1 px-4 py-6">
+                <div class="flex-none">
+                    <a href="###">
+                        <img src="https://source.unsplash.com/200x200/?face&crop=face&v=4" alt="avatar"
+                             class="w-14 h-14 rounded-lg">
+                    </a>
+                </div>
+
+                <div class="mx-4 w-full">
+                    <div class="text-gray-600">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem error iure magnam magni obcaecati quisquam. Lorem ipsum dolor sit amet, consectetur. Accusamus amet atque est harum, iste odio tempora? Eveniet, voluptatibus.
+                    </div>
+
+                    <div class="flex items-center justify-between mt-6">
+                        <div class="flex items-center text-xs font-semibold space-x-2 text-gray-400">
+                            <div class="font-bold text-gray-900">John Doe</div>
+                            <div>&bull;</div>
+                            <div>10 hours ago</div>
+                        </div>
+
+                        <div class="relative">
+                            <button
+                                class="bg-gray-100 hover:bg-gray-200 rounded-full border h-7 px-4 py-2 transition duration-150 ease-in">
+                                <svg class="text-gray-400 hover:text-gray-500" fill="currentColor" width="24"
+                                     height="6">
+                                    <path
+                                        d="M2.97.061A2.969 2.969 0 000 3.031 2.968 2.968 0 002.97 6a2.97 2.97 0 100-5.94zm9.184 0a2.97 2.97 0 100 5.939 2.97 2.97 0 100-5.939zm8.877 0a2.97 2.97 0 10-.003 5.94A2.97 2.97 0 0021.03.06z"></path>
+                                </svg>
+                            </button>
+                            <ul class="hidden absolute w-40 bg-white font-semibold shadow-dialog rounded-lg py-2 ml-12">
+                                <li>
+                                    <a href="#"
+                                       class="block hover:bg-gray-100 transition duration-150 ease-in px-5 py-2">Mark as
+                                        Spam</a>
+                                </li>
+                                <li>
+                                    <a href="#"
+                                       class="block hover:bg-gray-100 transition duration-150 ease-in px-5 py-2">Delete
+                                        Post</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div><!-- End comment container -->
+
+    </div><!-- End comments container -->
 </x-app-layout>
