b718b145c19e41f4fac2d2e9c1662df369e55788
Ian Travers <mr.itpe@gmail.com>
2021-04-07 09:28:12 +0300

    Idea page #1: start with all ideas link

---
+++ resources/views/show.blade.php
@@ -0,0 +1,10 @@
+<x-app-layout>
+    <div>
+        <a href="/" class="flex items-center font-semibold hover:underline">
+            <svg class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
+            </svg>
+            <span class="ml-2">All Ideas</span>
+        </a>
+    </div>
+</x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,6 @@ Route::get('/', function () {
     return view('index');
 });
 
+Route::view('/idea', 'show');
+
 require __DIR__.'/auth.php';
