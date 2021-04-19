401c3856e33ee3727236cc31f8c5ce698684f592
Ian Travers <mr.itpe@gmail.com>
2021-04-19 09:58:44 +0300

    Make a Livewire Component to Fetch Ideas

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -3,25 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Idea;
-use App\Models\Vote;
 
 class IdeaController extends Controller
 {
-    public function index()
-    {
-        return view('idea.index', [
-            'ideas' => Idea::with('user', 'category', 'status')
-                ->addSelect([
-                    'voted_by_current_user' => Vote::select('id')
-                        ->where('user_id', auth()->id())
-                        ->whereColumn('idea_id', 'ideas.id')
-                ])
-                ->withCount('votes')
-                ->latest()
-                ->simplePaginate(Idea::PAGINATION_COUNT),
-        ]);
-    }
-
     public function show(Idea $idea)
     {
         return view('idea.show', compact('idea'));
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,13 @@
 <?php
 
 use App\Http\Controllers\IdeaController;
+use App\Http\Livewire\IdeasIndex;
 use Illuminate\Support\Facades\Route;
 
 Route::group([
    'as' => 'idea',
 ], function () {
-    Route::get('/', [IdeaController::class, 'index'])->name('.index');
+    Route::get('/', IdeasIndex::class)->name('.index');
     Route::get('/ideas/{idea:slug}', [IdeaController::class, 'show'])->name('.show');
 });
 
